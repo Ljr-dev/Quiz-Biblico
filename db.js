f5bcaa0821ca235c134587ef60a6ff225ac56570
Leandro <leandrojoserocha@hotmail.com>
2026-04-05 00:49:03 -0300

    fix sqlite render

--- db.js
+++ db.js
@@ -1,30 +1,28 @@
-const sqlite3 = require("sqlite3").verbose();
+const Database = require("better-sqlite3");
 
-const db = new sqlite3.Database("./quiz.db");
+const db = new Database("quiz.db");
 
 // criar tabelas
-db.serialize(() => {
-  db.run(`
-    CREATE TABLE IF NOT EXISTS questions (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      category TEXT,
-      question TEXT,
-      option1 TEXT,
-      option2 TEXT,
-      option3 TEXT,
-      option4 TEXT,
-      correct INTEGER
-    )
-  `);
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS questions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    category TEXT,
+    question TEXT,
+    option1 TEXT,
+    option2 TEXT,
+    option3 TEXT,
+    option4 TEXT,
+    correct INTEGER
+  )
+`).run();
 
-  db.run(`
-    CREATE TABLE IF NOT EXISTS ranking (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      name TEXT,
-      score INTEGER,
-      created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-    )
-  `);
-});
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS ranking (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    score INTEGER,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+  )
+`).run();
 
 module.exports = db;
